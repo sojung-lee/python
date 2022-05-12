b3829c6e548c516a7243584eb6b860a6c9374b5f
sojung-lee <skyhigh01237@gmail.com>
2022-05-12 14:07:15 +0900

    .

---
+++ 5. 고급 정렬 알고리즘 - 01. 핵심 유형 문제풀이/11004. K번째 수.py
@@ -0,0 +1,32 @@
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
+    i, j, k = 0,0,0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            array[k] = left[i]
+            i += 1
+        else:
+            array[k] = right[j]
+            j += 1
+        k += 1
+    if i == len(left):
+        while j < len(right):
+            array[k] = right[j]
+            j += 1
+            k += 1
+    elif j == len(right):
+        while i < len(left):
+            array[k] = left[i]
+            i += 1
+            k += 1
+    return array
+
+n, k = map(int, input().split())
+array = list(map(int, input().split()))
+array = merge_sort(array)
+
+print(array[k-1])
